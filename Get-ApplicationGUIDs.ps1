9a443bec6afa669f09cd27995b8cf1db78a51535
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2022-01-31 23:10:24 -0500

    update Get-ApplicationGUIDs.ps1

--- Get-ApplicationGUIDs.ps1
+++ Get-ApplicationGUIDs.ps1
@@ -16,16 +16,16 @@
 
 function Find-AppIDs
 {
-|[CmdletBinding()]
-|param ()
+[CmdletBinding()]
+param ()
 
-|$UninstallKeys = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall", "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-|$null = New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS
-|$UninstallKeys += Get-ChildItem HKU: -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | ForEach-Object { "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall" }
-|foreach ($UninstallKey in $UninstallKeys)
-|{
-||Get-ChildItem -Path $UninstallKey -ErrorAction SilentlyContinue | Where { $_.PSChildName -match '^{[A-Z0-9]{8}-([A-Z0-9]{4}-){3}[A-Z0-9]{12}}$' } | Select-Object @{ n = 'GUID'; e = { $_.PSChildName } }, @{ n = 'Name'; e = { $_.GetValue('DisplayName') } }
-|}
+$UninstallKeys = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall", "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$null = New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS
+$UninstallKeys += Get-ChildItem HKU: -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | ForEach-Object { "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall" }
+foreach ($UninstallKey in $UninstallKeys)
+{
+Get-ChildItem -Path $UninstallKey -ErrorAction SilentlyContinue | Where { $_.PSChildName -match '^{[A-Z0-9]{8}-([A-Z0-9]{4}-){3}[A-Z0-9]{12}}$' } | Select-Object @{ n = 'GUID'; e = { $_.PSChildName } }, @{ n = 'Name'; e = { $_.GetValue('DisplayName') } }
+}
 }
 
 Find-AppIDs | Out-File -FilePath $PSScriptRoot\AppIDs.txt -encoding ASCII
